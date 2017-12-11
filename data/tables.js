d05ef2b5c577670d5307e2e63c06b51969920cdc
franklinskywalker <franklincruz336@gmail.com>
2017-12-11 13:26:29 -0500

    update to link on buttons, add a table route

--- data/tables.js
+++ data/tables.js
@@ -1,4 +1,1 @@
-[{ "reserved": false, "name": "0", "phone": "0", "id": "0", "email": "email0" }
-, { "reserved": false, "name": "1", "phone": "1", "id": "1", "email": "email1" }
-, { "name": "Eddie", "phone": "911", "id": "123123", "email": "eddie911@gmail.cm" }
-, { "name": "Ed", "phone": "919772339", "id": "1234", "email": "ed@uncc.edu" }]
+[{"reserved":false,"name":"0","phone":"0","id":"0","email":"email0"},{"reserved":false,"name":"1","phone":"1","id":"1","email":"email1"},{"name":"Eddie","phone":"911","id":"123123","email":"eddie911@gmail.cm"},{"name":"Ed","phone":"919772339","id":"1234","email":"ed@uncc.edu"},{"name":"f","phone":"1231234123","id":"566","email":"email@email.com"}]
